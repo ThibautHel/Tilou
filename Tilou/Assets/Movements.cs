d8c6657cf7f4bb1b27f69c2f5521f80e06dd1462
thibauthellinckx <thibaut.hellinckx@hotmail.com>
2023-06-12 10:45:33 +0200

    camere snapping and aiming

--- Tilou/Assets/Movements.cs
+++ Tilou/Assets/Movements.cs
@@ -1,38 +1,105 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(PlayerInputs))]
 public class Movements : MonoBehaviour
 {
     [SerializeField] private float Speed = 5f;
     [SerializeField] private float RotSpeed = 15f;
-    [SerializeField] private PlayerInputs playerInputs;
-    public CharacterController controller;
-    private float turnVelo;
-    private float turnSmoothTime = .1f;
-
-    public Transform cam;
+    [SerializeField] private PlayerInputs PlayerInputs;
+    public CharacterController Controller;
+    private float TurnVelo;
+    private float TurnSmoothTime = .1f;
+    public Transform MainCam;
+    public Transform AimCam;
+    public Transform ThirdPersonCam;
 
     private void Start()
     {
-        cam = Camera.main.transform;
+        MainCam = Camera.main.transform;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        PlayerInputs.OnAimChanged.AddListener(SetCamera);
+    }
+
+    private void Move()
+    {
+        if (!PlayerInputs.isAiming)
+        {
+            MoveWithoutAim();
+        }
+        else
+        {
+            MoveWithAim();
+        }
+    }
+    private void MoveWithAim()
+    {
+        Vector3 move_dir = PlayerInputs.MoveDir;
+        float mouse_x = Input.GetAxisRaw("Mouse X");
+        //transform.Rotate(Vector3.up * RotSpeed * Time.deltaTime * mouseX);
+        //AimCam.transform.Rotate(Vector3.up * RotSpeed * Time.deltaTime * mouse_x);
+
+        Vector3 cameraFwd = Camera.main.transform.forward;
+        Vector3 flatten_camera = new Vector3(cameraFwd.x, 0, cameraFwd.z).normalized;
+
+        float target_angle = Mathf.Atan2(flatten_camera.x, flatten_camera.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target_angle, ref TurnVelo, TurnSmoothTime);
+
+        //transform.rotation = Quaternion.LookRotation(flatten_camera, transform.up);
+        transform.rotation = Quaternion.AngleAxis(angle, transform.up);
+
+        transform.Translate(move_dir * Time.deltaTime * Speed);
+    }
+
+    private void MoveWithoutAim()
+    {
+        if (PlayerInputs.MoveDir.magnitude > 0)
+        {
+            float targetAngle = Mathf.Atan2(PlayerInputs.MoveDir.x, PlayerInputs.MoveDir.z) * Mathf.Rad2Deg + MainCam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref TurnVelo, TurnSmoothTime);
+            transform.rotation = Quaternion.AngleAxis(angle, transform.up);
+            Debug.Log(angle);
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            transform.Translate(moveDir.normalized * Time.deltaTime * Speed, Space.World);
+        }
+    }
+
+    private void SetCamera()
+    {
+        if (PlayerInputs.isAiming)
+        {
+            /* Vector3 camera_dir = Camera.main.transform.forward;
+             Vector3 camera_dir_local = transform.InverseTransformVector(camera_dir);
+             */
+            
+
+            ThirdPersonCam.gameObject.SetActive(false);
+            AimCam.gameObject.SetActive(true);
+        }
+        else if (!PlayerInputs.isAiming)
+        {
+            AimCam.gameObject.SetActive(false);
+            ThirdPersonCam.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 cameraFwd = Camera.main.transform.forward;
+        Vector3 flatten_camera_dir = new Vector3(cameraFwd.x, 0, cameraFwd.z).normalized;
+
+        Gizmos.DrawRay(transform.position, flatten_camera_dir *100);
     }
 
     void Update()
     {
-
         //transform.Rotate(Vector3.up,playerInputs.RotationDir * RotSpeed * Time.deltaTime);
-        if (playerInputs.MoveDir.magnitude > 0)
-        {
-            float targetAngle = Mathf.Atan2(playerInputs.MoveDir.x, playerInputs.MoveDir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelo, turnSmoothTime);
-            transform.rotation = Quaternion.AngleAxis(angle, transform.up);
 
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            transform.Translate( moveDir.normalized * Time.deltaTime * Speed, Space.World);
-            Debug.Log(moveDir);
-        }
+        Move();
+    }
+
+    private void OnDestroy()
+    {
+        PlayerInputs.OnAimChanged.RemoveListener(SetCamera);
     }
 }
--- Tilou/Assets/PlayerInputs.cs
+++ Tilou/Assets/PlayerInputs.cs
@@ -1,13 +1,44 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInputs : MonoBehaviour
 {
     public Vector3 MoveDir { get; private set; } = Vector3.zero;
     public float RotationDir { get; private set; }
     public float AngleInRad { get; private set; }
+    public bool isAiming
+    {
+        get
+        {
+            return IsAiming;
+        }
+        private set
+        {
+            IsAiming = value;
+            OnAimChanged.Invoke();
+        }
+    }
+
+    private bool IsAiming = false;
+
+    public UnityEvent OnAimChanged;
 
     private void Update()
     {
         MoveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (Input.GetMouseButton(1))
+        {
+            if (!IsAiming)
+            {
+                isAiming = true;
+            }
+        }
+        else
+        {
+            if (isAiming)
+            {
+                isAiming = false;
+            }
+        }
     }
 }
